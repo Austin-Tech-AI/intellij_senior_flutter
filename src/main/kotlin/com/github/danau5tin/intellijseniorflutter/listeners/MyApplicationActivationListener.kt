1784373896f4a34f2c03df4361e4ae4992417209
GitHub Action <action@github.com>
2023-06-19 05:48:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/danau5tin/intellijseniorflutter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/danau5tin/intellijseniorflutter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.danau5tin.intellijseniorflutter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
