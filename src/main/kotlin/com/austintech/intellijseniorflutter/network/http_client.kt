fbb08d74a2d24964891cee00b4689125b52f788c
Daniel Austin <dan96.austin@gmail.com>
2023-06-20 22:49:46 +0200

    feat: Increased timeout

--- src/main/kotlin/com/austintech/intellijseniorflutter/network/http_client.kt
+++ src/main/kotlin/com/austintech/intellijseniorflutter/network/http_client.kt
@@ -3,5 +3,5 @@ package com.austintech.intellijseniorflutter.network
 import okhttp3.OkHttpClient
 
 object HttpClient {
-    val client = OkHttpClient().newBuilder().readTimeout(60, java.util.concurrent.TimeUnit.SECONDS).build()
+    val client = OkHttpClient().newBuilder().readTimeout(300, java.util.concurrent.TimeUnit.SECONDS).build()
 }
